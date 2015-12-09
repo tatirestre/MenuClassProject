d2f5d4c84a41995b23d84b9341a8cbef8189c584
Tatiana Restrepo <trestrepo@lat-trestrepo.local>
2015-12-09 00:23:49 -0500

    added friendly ID to restaurants

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'simple_form'
 gem 'devise'
 gem 'geocoder'
+gem 'friendly_id', '~> 5.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,17 @@
 class Restaurant < ActiveRecord::Base
 
+#Friendly ID code
+extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders] 
+
+
+# GeoCoder - Map View code:
 geocoded_by :address
 after_validation :geocode
 
  def address
  "#{street_line_1}, #{city}, #{state}"
  end
-
+# end of Geocoder
 
 end
